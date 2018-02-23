65797ebd0d20f52fca5e497fba97271354bcc8b9
Graham Fairweather <xotic750@gmail.com>
2018-02-23 23:51:31 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Test if a given value is truthy.
- * @version 1.0.0
+ * @version 1.0.1
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
