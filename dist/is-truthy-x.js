fcd4c5b60c127110bd979f71813b949de451aa7b
Graham Fairweather <xotic750@gmail.com>
2019-07-31 21:23:23 +0200

    :bookmark: v2.0.23
    Auto build

--- dist/is-truthy-x.js
+++ dist/is-truthy-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-31T17:07:23.710Z",
+  "date": "2019-07-31T19:23:18.933Z",
   "describe": "",
   "description": "Test if a given value is truthy.",
   "file": "is-truthy-x.js",
